5e3436053ac6d57df9d106475d267c168f4d28af
sn1ch <sn1ch@list.ru>
2019-08-09 20:14:42 +0300

    add bd modul

---
+++ bd/bd.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+
+
+def start_bd():
+    client = MongoClient()
+    VK_db = client['VK_db']
+    skip_ids = VK_db['skip_ids211']
+    top10_users_mongo = VK_db['top10users211']
+    skip_ids.insert_one({'ID': [0]})
+    return skip_ids, top10_users_mongo
+
+
+def get_skip_ids_list(skip_ids):
+    skip_ids_list = skip_ids.find_one()['ID']
+    return skip_ids_list
+
+
+def write_users_in_skip_id_bd(top10_users, skip_ids):
+    for id in top10_users:
+        skip_ids.update_one({'ID': skip_ids.find_one()['ID']}, {'$push': {'ID': id}})
+
+
+def write_top10users_bd(data, top10_users_mongo):
+    top10_users_mongo.insert_one({'users': data})
